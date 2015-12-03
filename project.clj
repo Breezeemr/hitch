48e03982449ec2f87807f9472e9f8356353abe8e
ttallman <dspiteself@gmail.com>
2015-12-03 11:16:13 -0600

    bump version so I can deploy

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.breezeehr/hitch "0.1.2-SNAPSHOT"
+(defproject com.breezeehr/hitch "0.1.3-SNAPSHOT"
   :description "A Clojurescript library designed to manage and cache derived data."
   :url "https://github.com/Breezeemr/hitch"
   :license {:name "Eclipse Public License"
