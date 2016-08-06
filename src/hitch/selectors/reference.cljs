6bb71fafdcda490c0e302b526026191e3f956ab0
ttallman <dspiteself@gmail.com>
2016-08-06 14:41:26 -0500

    rework reference

--- src/hitch/selectors/reference.cljs
+++ src/hitch/selectors/reference.cljs
@@ -3,21 +3,22 @@
             [hitch.graph :as graph]
             [hitch.selector :refer-macros [defselector]]))
 
-(defselector reference [graph k]
-             (when-let [selector (and proto/*current-node* (.-refs proto/*current-node*))]
-                (<! (graph/get-or-create-node! graph selector))))
+(defrecord Reference [n]
+  proto/StatefulSelector
+  (init [selector] nil)
+  (clear [selector state])
+  proto/SelectorEffects
+  (-apply [selector old-state effect]
+    (let [[key newvalue] effect]
+      (case key
+        :set-value [(second effect)])))
+  proto/SelectorValue
+  (-value [selector graph state]
+    (graph/get-or-create-node! graph state)))
 
-(defn set-reference!
-  ([graph k data-selector]
-   (when data-selector
-     (let [kselector (proto/-selector reference graph k)
-           reference-node (graph/try-to-get-node graph kselector)
-           data-selector-node (graph/get-or-create-node! graph data-selector)]
-       (when reference-node
-         (when (.-refs reference-node)
-           (proto/undepend! graph reference-node data-selector-node))
-         (set! (.-refs reference-node) data-selector-node)
-         (proto/depend! graph reference-node data-selector-node)
-         (graph/invalidate-nodes graph [reference-node]))
-       ))))
+(def reference
+  (reify proto/ISelectorFactory
+    (-selector [_ n]
+      (->Reference n))))
+
 
