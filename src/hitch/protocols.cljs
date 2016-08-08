bc5221c50edbdeca5fa03c8d29d75910bd1ff5bb
ttallman <dspiteself@gmail.com>
2016-08-08 15:53:25 -0500

    remove createnode protocol

--- src/hitch/protocols.cljs
+++ src/hitch/protocols.cljs
@@ -18,11 +18,6 @@
 (defprotocol ISelectorFactory
   (-selector [selector-factory] [selector-factory a] [selector-factory a b] [selector-factory a b c] [selector-factory a b c d] [selector-factory a b c d e] [selector-factory a b c d e f] [selector-factory a b c d e f g] [selector-factory a b c d e f g h]))
 
-(defprotocol ICreateNode
-  (-create-node [this graph]))
-
-
-
 (defprotocol IDynamicDepNode
   (get-tx [this])
   (set-tx! [this tx]))
