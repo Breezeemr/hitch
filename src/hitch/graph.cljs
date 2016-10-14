56087be36776f0de14939bf887b8fd476010c93d
ttallman <dspiteself@gmail.com>
2016-10-14 11:21:41 -0500

    update the external api

--- src/hitch/graph.cljs
+++ src/hitch/graph.cljs
@@ -37,17 +37,7 @@
 (defn try-to-get-node [dependency-graph data-selector]
   (get dependency-graph data-selector))
 
-(defn hitch-node
-  ([graph selector-constructor] (dget! graph (selector-constructor)))
-  ([graph selector-constructor a] (dget! graph (selector-constructor a)))
-  ([graph selector-constructor a b] (dget! graph (selector-constructor a b)))
-  ([graph selector-constructor a b c] (dget! graph (selector-constructor a b c)))
-  ([graph selector-constructor a b c d] (dget! graph (selector-constructor a b c d)))
-  ([graph selector-constructor a b c d f] (dget! graph (selector-constructor a b c d f)))
-  ([graph selector-constructor a b c d f g] (dget! graph (selector-constructor a b c d f g)))
-  ([graph selector-constructor a b c d f g h] (dget! graph (selector-constructor a b c d f g h))))
-
-(defn hook-node
+(defn hook
   ([graph selector-constructor] (make-hook graph (selector-constructor)))
   ([graph selector-constructor a] (make-hook graph (selector-constructor a)))
   ([graph selector-constructor a b] (make-hook graph (selector-constructor a b)))
@@ -57,45 +47,73 @@
   ([graph selector-constructor a b c d f g] (make-hook graph (selector-constructor a b c d f g)))
   ([graph selector-constructor a b c d f g h] (make-hook graph (selector-constructor a b c d f g h))))
 
-(defn hitch-eval
-  ([graph selector-constructor] (oldproto/inline selector-constructor graph))
-  ([graph selector-constructor a] (oldproto/inline selector-constructor graph a))
-  ([graph selector-constructor a b] (oldproto/inline selector-constructor graph a b))
-  ([graph selector-constructor a b c] (oldproto/inline selector-constructor graph a b c))
-  ([graph selector-constructor a b c d] (oldproto/inline selector-constructor graph a b c d))
-  ([graph selector-constructor a b c d f] (oldproto/inline selector-constructor graph a b c d f))
-  ([graph selector-constructor a b c d f g] (oldproto/inline selector-constructor graph a b c d f g))
-  ([graph selector-constructor a b c d f g h] (oldproto/inline selector-constructor graph a b c d f g h)))
+(defn hitch*!
+  ([graph nf selector-constructor]
+   (if *execution-mode*
+     (dget! graph (selector-constructor) nf)
+     (oldproto/inline selector-constructor graph)))
+  ([graph nf selector-constructor a]
+   (if *execution-mode*
+     (dget! graph (selector-constructor a) nf)
+     (oldproto/inline selector-constructor graph a)))
+  ([graph nf selector-constructor a b]
+   (if *execution-mode*
+     (dget! graph (selector-constructor a b) nf)
+     (oldproto/inline selector-constructor graph a b)))
+  ([graph nf selector-constructor a b c]
+   (if *execution-mode*
+     (dget! graph (selector-constructor a b c) nf)
+     (oldproto/inline selector-constructor graph a b c)))
+  ([graph nf selector-constructor a b c d]
+   (if *execution-mode*
+     (dget! graph (selector-constructor a b c d) nf)
+     (oldproto/inline selector-constructor graph a b c d)))
+  ([graph nf selector-constructor a b c d e]
+   (if *execution-mode*
+     (dget! graph (selector-constructor a b c d e) nf)
+     (oldproto/inline selector-constructor graph a b c d e)))
+  ([graph nf selector-constructor a b c d e f ]
+   (if *execution-mode*
+     (dget! graph (selector-constructor a b c d e f ) nf)
+     (oldproto/inline selector-constructor graph a b c d e f )))
+  ([graph nf selector-constructor a b c d e f g ]
+   (if *execution-mode*
+     (dget! graph (selector-constructor a b c d e f g) nf)
+     (oldproto/inline selector-constructor graph a b c d e f g))))
 
-
-
-(defn hitch
+(defn hitch!
   ([graph selector-constructor]
-   ((if *execution-mode* hitch-node hitch-eval) graph selector-constructor))
+   (if *execution-mode*
+     (dget! graph (selector-constructor))
+     (oldproto/inline selector-constructor graph)))
   ([graph selector-constructor a]
-   ((if *execution-mode* hitch-node hitch-eval) graph selector-constructor a))
+   (if *execution-mode*
+     (dget! graph (selector-constructor a))
+     (oldproto/inline selector-constructor graph a)))
   ([graph selector-constructor a b]
-   ((if *execution-mode* hitch-node hitch-eval) graph selector-constructor a b))
+   (if *execution-mode*
+     (dget! graph (selector-constructor a b))
+     (oldproto/inline selector-constructor graph a b)))
   ([graph selector-constructor a b c]
-   ((if *execution-mode* hitch-node hitch-eval) graph selector-constructor a b c))
+   (if *execution-mode*
+     (dget! graph (selector-constructor a b c))
+     (oldproto/inline selector-constructor graph a b c)))
   ([graph selector-constructor a b c d]
-   ((if *execution-mode* hitch-node hitch-eval) graph selector-constructor a b c d))
-  ([graph selector-constructor a b c d f]
-   ((if *execution-mode* hitch-node hitch-eval) graph selector-constructor a b c d f))
-  ([graph selector-constructor a b c d f g]
-   ((if *execution-mode* hitch-node hitch-eval) graph selector-constructor a b c d f g))
-  ([graph selector-constructor a b c d f g h]
-   ((if *execution-mode* hitch-node hitch-eval) graph selector-constructor a b c d f g h)))
-
-(defn hook
-  ([graph selector-constructor] ((if *execution-mode* hook-node hitch-eval) graph selector-constructor))
-  ([graph selector-constructor a] ((if *execution-mode* hook-node hitch-eval) graph selector-constructor a))
-  ([graph selector-constructor a b] ((if *execution-mode* hook-node hitch-eval) graph selector-constructor a b))
-  ([graph selector-constructor a b c] ((if *execution-mode* hook-node hitch-eval) graph selector-constructor a b c))
-  ([graph selector-constructor a b c d] ((if *execution-mode* hook-node hitch-eval) graph selector-constructor a b c d))
-  ([graph selector-constructor a b c d f] ((if *execution-mode* hook-node hitch-eval) graph selector-constructor a b c d f))
-  ([graph selector-constructor a b c d f g] ((if *execution-mode* hook-node hitch-eval) graph selector-constructor a b c d f g))
-  ([graph selector-constructor a b c d f g h] ((if *execution-mode* hook-node hitch-eval) graph selector-constructor a b c d f g h)))
+   (if *execution-mode*
+     (dget! graph (selector-constructor a b c d))
+     (oldproto/inline selector-constructor graph a b c d)))
+  ([graph selector-constructor a b c d e]
+   (if *execution-mode*
+     (dget! graph (selector-constructor a b c d e))
+     (oldproto/inline selector-constructor graph a b c d e)))
+  ([graph selector-constructor a b c d e f ]
+   (if *execution-mode*
+     (dget! graph (selector-constructor a b c d e f ))
+     (oldproto/inline selector-constructor graph a b c d e f )))
+  ([graph selector-constructor a b c d e f g ]
+   (if *execution-mode*
+     (dget! graph (selector-constructor a b c d e f g))
+     (oldproto/inline selector-constructor graph a b c d e f g))))
 
 (defn apply-effects [graph selector-effect-pairs]
   (oldproto/apply-commands graph selector-effect-pairs))
