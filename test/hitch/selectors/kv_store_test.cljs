29539d2782434e2e1ba42499974520c731b6a4f5
Francis Avila <favila@breezeehr.com>
2016-10-25 17:16:52 -0500

    kv_store tests now run on both graph types, all tests passing

--- test/hitch/selectors/kv_store_test.cljs
+++ test/hitch/selectors/kv_store_test.cljs
@@ -1,41 +1,55 @@
 (ns hitch.selectors.kv-store-test
-  (:require-macros [hitch.selector :refer [defselector]]
-                   [devcards.core :as dc :refer [deftest]])
-  (:require [cljs.test :refer [] :refer-macros [is run-tests async]]
-            [hitch.core :as core]
-            [hitch.mutable.node :as simple]
-            [hitch.protocol :as proto]
-            [hitch.selectors.kv-store :as kv :refer [keyspace key]]
+  (:require-macros )
+  (:require [cljs.test :refer-macros [is async]]
+            [hitch.selector :refer-macros [defselector]]
+            [devcards.core :refer-macros [deftest]]
+            [hitch.selectors.kv-store :as kv :refer [keyspace keystore-get]]
             [hitch.graph :as graph]
-            [hitch.mutable.graph :as mgraph]))
+            [hitch.pin :refer [pin]]
+            [hitch.mutable.graph :as mgraph]
+            [hitch.graphs.graph-manager :as gm]
+            [hitch.graphs.immutable :as im]))
 
+(def gctors
+  [["Mutable graph: " mgraph/graph]
+   ["Immutable graph: "
+    #(-> (gm/atom-GraphManager (im/->ImmutableGraph 1) gm/synchronous-watcher
+           identity)
+         :graph-manager gm/ilookup+depgraph-facade)]])
 
-(deftest firstt
-  (let [graph (mgraph/graph)]
-    (async done
-      (let [ks-sel (keyspace :main)
-            node (graph/hook graph (fn [v])
-                        kv/key ks-sel :test)]
-        (is (= (get graph (kv/key ks-sel :test) nil) nil))
-        (graph/apply-commands graph [[ks-sel [:set-value {:test :cat}]]])
-        (graph/hook graph (fn [v]
-                            (is (= v :cat))
-                            (done))
-                    kv/key ks-sel :test)))))
+(doseq [[graph-name gctor] gctors]
+  (deftest async-hook-value-change
+    (let [graph (gctor)]
+      (async done
+        (let [ks-sel (keyspace :main)]
+          (is (= (get graph (kv/keystore-get ks-sel :test) nil) nil)
+            (str graph-name "nil value before keystore is created."))
+          (pin graph (kv/keystore-get ks-sel :test))
+          (is (= (get graph (kv/keystore-get ks-sel :test) nil) nil)
+            (str graph-name "still nil value before keystore has a value."))
+          (graph/apply-commands graph [[ks-sel [:set-value {:test :cat}]]])
+          (graph/hook graph (fn [v]
+                              (is (= v :cat)
+                                (str graph-name "Keystore has a value"))
+                              (done))
+            kv/keystore-get ks-sel :test)))))
 
-(deftest firstasync
-  (let [graph (mgraph/graph)
-        testsel (keyspace :main)
-        keysel (key testsel :test)
-        ]
-    (is (= (get graph keysel :not-found) :not-found))
-    (graph/hook graph (fn [val]) key testsel :test)
-    (is (= (get graph keysel :not-found) nil))
-    (graph/apply-commands graph [[testsel [:set-value {:test 7}]]])
-    (is (= (get graph keysel) 7))
-    (graph/apply-commands graph [[testsel [:set-value {:test 8}]]])
-    (is (= (get graph keysel) 8))
-    (graph/apply-commands graph [[testsel [:set-value {:test 9}]]])
-    (is (= (get graph keysel) 9))
-    ))
+  (deftest sync-value-changes
+    (let [graph   (gctor)
+          testsel (keyspace :main)
+          keysel  (keystore-get testsel :test)]
+      (is (= (get graph keysel :not-found) :not-found)
+        (str graph-name "not-found value before keystore is created."))
+      (pin graph keysel)
+      (is (= (get graph keysel :not-found) nil)
+        (str graph-name "nil value after keystore is created"))
+      (graph/apply-commands graph [[testsel [:set-value {:test 7}]]])
+      (is (= (get graph keysel) 7)
+        (str graph-name "keystore-get value updated syncronously first time"))
+      (graph/apply-commands graph [[testsel [:set-value {:test 8}]]])
+      (is (= (get graph keysel) 8)
+        (str graph-name "keystore-get value updated syncronously second time"))
+      (graph/apply-commands graph [[testsel [:set-value {:test 9}]]])
+      (is (= (get graph keysel) 9)
+        (str graph-name "keystore-get value updated syncronously third time")))))
 
