5f464dd7bcafcdb75ff590cea61d27fcfe9aa397
Francis Avila <francisga@gmail.com>
2017-05-08 23:46:49 -0500

    Make defselector usable from Clojure

--- src/hitch/selector.clj
+++ src/hitch/selector.clj
@@ -1,40 +1,47 @@
 (ns hitch.selector
-    (:require [cljs.core ]
+    (:require
+      [cljs.core]
       [clojure.walk :refer [postwalk]]
       [clojure.string :as str]))
 
+(defn cljs-target? [env]
+  (some? (:ns env)))
+
 (defn eval-selector [eval-fn-name constructor-binding-forms body]
-  `(cljs.core/defn ~eval-fn-name ~(into [] (map second) constructor-binding-forms)
+  `(defn ~eval-fn-name ~(into [] (map second) constructor-binding-forms)
      ~@body))
 
 (defn selector-record [selector-name eval-fn-name constructor-binding-forms]
   (let [graphsymbol (->> constructor-binding-forms ffirst)]
-    `(cljs.core/defrecord ~selector-name ~(into [] (map first) (rest constructor-binding-forms))
+    `(defrecord ~selector-name ~(into [] (map first) (rest constructor-binding-forms))
        hitch.protocol/Selector
        (~'value [~'selector ~graphsymbol ~'state]
-         (cljs.core/let [~'dtx (hitch.selector-tx-manager/tx ~graphsymbol ~'selector)]
+         (let [~'dtx (hitch.selector-tx-manager/tx ~graphsymbol ~'selector)]
            (hitch.selector/attempt ~eval-fn-name ~'dtx
              ~@(map first (rest constructor-binding-forms))))))))
 
-(defn sel-constructor [name eval-fn-name selector-name constructor-binding-forms body]
+(defn sel-constructor
+  [env name eval-fn-name selector-name constructor-binding-forms body]
   `(def ~name
-     (cljs.core/reify
-       hitch.protocol/SelectorName
-       (~'sel-name  [~'s]
-         ~(str *ns* "/" selector-name))
+     (reify
        hitch.oldprotocols/ISelectorFactory
        (~'inline ~(into ['this] (map first) constructor-binding-forms)
-         (cljs.core/assert (cljs.core/satisfies? hitch.oldprotocols/IDependencyGraph ~(ffirst constructor-binding-forms)))
+         (assert (satisfies? hitch.oldprotocols/IDependencyGraph ~(ffirst constructor-binding-forms)))
          ~(->> constructor-binding-forms
                             (map first)
                             (cons eval-fn-name)))
-       cljs.core/IFn
-       (~'-invoke ~(into ['this] (map first) (rest constructor-binding-forms))
+       ~(if (cljs-target? env)
+          'cljs.core/IFn
+          'clojure.lang.IFn)
+       (~(if (cljs-target? env)
+           '-invoke
+           'invoke)
+         ~(into ['this] (map first) (rest constructor-binding-forms))
          ~(->> constructor-binding-forms
                             (map first)
                             rest
-                            (cons (symbol (str "->" selector-name)))))
-       )))
+                            (cons (symbol (str "->" selector-name))))))))
+
 (defn create-binding-syms [binding-form]
   (mapv (juxt gensym identity) binding-form))
 
@@ -45,5 +52,4 @@
     `(do
        ~(eval-selector eval-fn-name symbol-binding-pairs body)
        ~(selector-record selector-name eval-fn-name symbol-binding-pairs)
-       ~(sel-constructor name eval-fn-name selector-name symbol-binding-pairs body)
-       )))
+       ~(sel-constructor &env name eval-fn-name selector-name symbol-binding-pairs body))))
--- test/hitch/core_test.clj
+++ test/hitch/core_test.clj
@@ -5,20 +5,20 @@
 (deftest selectors
   (let [binding-pairs '[[G__1 graph] [G__2 a]]]
     (is (= (sel/eval-selector 'evaler binding-pairs ())
-           '(cljs.core/defn evaler [graph a])))
+           '(clojure.core/defn evaler [graph a])))
     (is (= (sel/selector-record 'evaler-rec 'evaler-fn binding-pairs)
-           '(cljs.core/defrecord evaler-rec [G__2]
+           '(clojure.core/defrecord evaler-rec [G__2]
               hitch.protocol/Selector
               (value [selector G__1 state]
-                (cljs.core/let [dtx (hitch.selector-tx-manager/tx G__1 selector)]
+                (clojure.core/let [dtx (hitch.selector-tx-manager/tx G__1 selector)]
                   (hitch.selector/attempt evaler-fn dtx G__2))))))
-    (is (= (sel/sel-constructor 'evaler 'evaler-fn 'evaler-rec binding-pairs ())
+    (is (= (sel/sel-constructor nil 'evaler 'evaler-fn 'evaler-rec binding-pairs ())
            '(def evaler
-              (cljs.core/reify
+              (clojure.core/reify
                 hitch.oldprotocols/ISelectorFactory
                 (inline [this G__1 G__2]
-                  (cljs.core/assert (cljs.core/satisfies? hitch.oldprotocols/IDependencyGraph G__1))
+                  (clojure.core/assert (clojure.core/satisfies? hitch.oldprotocols/IDependencyGraph G__1))
                   (evaler-fn G__1 G__2))
-                cljs.core/IFn
-                (-invoke[this G__2]
+                clojure.lang.IFn
+                (invoke[this G__2]
                   (->evaler-rec G__2))))))))
