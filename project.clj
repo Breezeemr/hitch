0981cc5d5771368e4f5b2eca397a33d1f2ba22f2
Francis Avila <favila@breezeehr.com>
2017-05-12 17:03:40 -0500

    Version 0.1.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.breezeehr/hitch "0.1.6-SNAPSHOT"
+(defproject com.breezeehr/hitch "0.1.6"
   :description "A Clojurescript library designed to manage and cache derived data."
   :url "https://github.com/Breezeemr/hitch"
   :license {:name "Eclipse Public License"
