40cf33734aa70865820948c00bf1887685affcd8
Francis Avila <favila@breezeehr.com>
2018-03-05 16:04:25 -0600

    Test mutable graph gc when mark becomes not-sweepable

--- test/hitch/graphs/mutable_test.cljs
+++ test/hitch/graphs/mutable_test.cljs
@@ -27,7 +27,9 @@
                             )
           http/http "/test.txt" :get nil nil nil nil nil)))))
 
-(deftest gc
+(deftest gc-clean-mark-sweep
+
+  ;; Tests gc when the marked nodes are still sweepable at sweep time
   (let [graph (mgraph/graph)
         mvar  (mutable-var :test)
         first-var (kv/get mvar 0)
@@ -41,26 +43,90 @@
     (is (= (get graph ff-var) [5]))
     (testing "unpinned"
       (unpin graph ff-var)
-      (is (= (get graph (mutable-var :test) :hitch.graphs.mutable-test/not-found) [[[5]]]))
-      (is (= (get graph first-var :hitch.graphs.mutable-test/not-found) [[5]]))
-      (is (= (get graph ff-var :hitch.graphs.mutable-test/not-found) [5])))
+      (is (= (get graph (mutable-var :test) ::not-found) [[[5]]]))
+      (is (= (get graph first-var ::not-found) [[5]]))
+      (is (= (get graph ff-var ::not-found) [5])))
 
     (testing "first pass"
       (.gc-pass graph)
-      (is (= (get graph (mutable-var :test) :hitch.graphs.mutable-test/not-found) [[[5]]]))
-      (is (= (get graph first-var :hitch.graphs.mutable-test/not-found) [[5]]))
-      (is (= (get graph ff-var :hitch.graphs.mutable-test/not-found) :hitch.graphs.mutable-test/not-found)))
+      (is (= (get graph (mutable-var :test) ::not-found) [[[5]]]))
+      (is (= (get graph first-var ::not-found) [[5]]))
+      (is (= (get graph ff-var ::not-found) ::not-found)))
     (testing "second pass"
       (.gc-pass graph)
-      (is (= (get graph (mutable-var :test) :hitch.graphs.mutable-test/not-found) [[[5]]]))
-      (is (= (get graph first-var :hitch.graphs.mutable-test/not-found) :hitch.graphs.mutable-test/not-found))
-      (is (= (get graph ff-var :hitch.graphs.mutable-test/not-found) :hitch.graphs.mutable-test/not-found)))
+      (is (= (get graph (mutable-var :test) ::not-found) [[[5]]]))
+      (is (= (get graph first-var ::not-found) ::not-found))
+      (is (= (get graph ff-var ::not-found) ::not-found)))
     (testing "third pass"
       (.gc-pass graph)
-      (is (= (get graph (mutable-var :test) :hitch.graphs.mutable-test/not-found) :hitch.graphs.mutable-test/not-found))
-      (is (= (get graph first-var :hitch.graphs.mutable-test/not-found) :hitch.graphs.mutable-test/not-found))
-      (is (= (get graph ff-var :hitch.graphs.mutable-test/not-found) :hitch.graphs.mutable-test/not-found)))))
+      (is (= (get graph (mutable-var :test) ::not-found) ::not-found))
+      (is (= (get graph first-var ::not-found) ::not-found))
+      (is (= (get graph ff-var ::not-found) ::not-found)))))
 
+(deftest gc-dirty-mark
+  ;; Tests gc when a marked node becomes not-sweepable by time sweep occurs
+  (let [graph     (mgraph/graph)
+        mvar      (mutable-var :test)
+        first-var (kv/get mvar 0)
+        ff-var    (kv/get first-var 0)]
+    (is (= (get graph (mutable-var :test) ::not-found) ::not-found))
+    (pin graph ff-var)
+    (is (= (get graph (mutable-var :test) ::not-found) ::not-found))
+    (graph/apply-commands graph [[(mutable-var :test) [:set-value [[[5]]]]]])
+    (is (= (get graph (mutable-var :test)) [[[5]]]))
+    (is (= (get graph first-var) [[5]]))
+    (is (= (get graph ff-var) [5]))
+    (testing "unpinned"
+      (unpin graph ff-var)
+      (is (= (get graph (mutable-var :test) ::not-found) [[[5]]]))
+      (is (= (get graph first-var ::not-found) [[5]]))
+      (is (= (get graph ff-var ::not-found) [5])))
+    (testing "first pass"
+      (.gc-pass graph)
+      (is (= (get graph (mutable-var :test) ::not-found) [[[5]]]))
+      (is (= (get graph first-var ::not-found) [[5]]))
+      (is (= (get graph ff-var ::not-found) ::not-found)))
+    (testing "second pass"
+      (.gc-pass graph)
+      (is (= (get graph (mutable-var :test) ::not-found) [[[5]]]))
+      (is (= (get graph first-var ::not-found) ::not-found))
+      (is (= (get graph ff-var ::not-found) ::not-found)))
+
+    (testing "repin at last moment: "
+      (pin graph ff-var)
+      (.gc-pass graph)
+      (is (= (get graph (mutable-var :test)) [[[5]]]))
+      (is (= (get graph first-var) [[5]]))
+      (is (= (get graph ff-var) [5])))
+
+    (testing "still pinned"
+      (.gc-pass graph)
+      (is (= (get graph (mutable-var :test)) [[[5]]]))
+      (is (= (get graph first-var) [[5]]))
+      (is (= (get graph ff-var) [5])))
+
+    (testing "unpinned again"
+      (unpin graph ff-var)
+      (is (= (get graph (mutable-var :test) ::not-found) [[[5]]]))
+      (is (= (get graph first-var ::not-found) [[5]]))
+      (is (= (get graph ff-var ::not-found) [5])))
+    (testing "first pass again"
+      (.gc-pass graph)
+      (is (= (get graph (mutable-var :test) ::not-found) [[[5]]]))
+      (is (= (get graph first-var ::not-found) [[5]]))
+      (is (= (get graph ff-var ::not-found) ::not-found)))
+    (testing "second pass again"
+      (.gc-pass graph)
+      (is (= (get graph (mutable-var :test) ::not-found) [[[5]]]))
+      (is (= (get graph first-var ::not-found) ::not-found))
+      (is (= (get graph ff-var ::not-found) ::not-found)))
+
+    (testing "fully collected"
+      (.gc-pass graph)
+      (is (= (get graph (mutable-var :test) ::not-found) ::not-found))
+      (is (= (get graph first-var ::not-found) ::not-found))
+      (is (= (get graph ff-var ::not-found) ::not-found)))
+    ))
 
 
 
